329e30aab6824673ecf9f65356365ff4dec2096c
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-09 15:58:05 -0300

    feat: add function for adding new product to menu

--- management/product_handler.py
+++ management/product_handler.py
@@ -1,4 +1,5 @@
 from menu import products
+from operator import itemgetter
 
 
 def get_product_by_id(id: int):
@@ -14,3 +15,16 @@ def get_products_by_type(type: str):
         if product['type'] == type:
             found_products.append(product)
     return found_products
+
+
+def add_product(menu: list, **kwargs: dict) -> dict:
+    id = 0
+    if len(menu) == 0:
+        id = 1
+    else:
+        sorted_list = sorted(menu, key=itemgetter('_id'))
+        last_product = sorted_list[-1]
+        id = last_product['_id'] + 1
+    kwargs['_id'] = id
+    menu.append(kwargs)
+    return kwargs
