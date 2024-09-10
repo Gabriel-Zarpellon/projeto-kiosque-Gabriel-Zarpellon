40e035ae72615fbd046a25ec4fbaf7cfea1c5490
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-10 15:11:46 -0300

    style: indent

--- management/tab_handler.py
+++ management/tab_handler.py
@@ -3,10 +3,13 @@ from menu import products
 
 def calculate_tab(tab: list) -> dict:
     total = 0
+
     for item in tab:
         id = item['_id']
         amount = item['amount']
         product = list(product for product in products if product['_id'] == id)
         total = round(total + (product[0]['price'] * amount), 2)
+
     subtotal = {'subtotal': f'${total}'}
+
     return subtotal
