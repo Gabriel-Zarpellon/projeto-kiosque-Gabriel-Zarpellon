f983a75cfec4b157a81e6d2b3f92db5ab806d667
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-09-10 15:13:46 -0300

    feat: add menu_report and add_product_extra functions

--- management/product_handler.py
+++ management/product_handler.py
@@ -28,12 +28,63 @@ def get_products_by_type(search_type: str) -> list:
 
 def add_product(menu: list, **kwargs: dict) -> dict:
     id = 0
+
     if len(menu) == 0:
         id = 1
     else:
         sorted_list = sorted(menu, key=itemgetter('_id'))
         last_product = sorted_list[-1]
         id = last_product['_id'] + 1
+
     kwargs['_id'] = id
     menu.append(kwargs)
+
+    return kwargs
+
+
+def menu_report() -> str:
+    product_count = len(products)
+    total_price = 0
+
+    for product in products:
+        total_price = total_price + product['price']
+    average_price = round(total_price / product_count, 2)
+    types = {'bakery': 0, 'dairy': 0, 'drink': 0, 'fruit': 0, 'vegetable': 0}
+
+    for product in products:
+        for key in types:
+            if key == product['type']:
+                types[key] = types[key] + 1
+
+    most_common_type = {'most_common': 0, 'type': ''}
+
+    for key, value in types.items():
+        if value > most_common_type['most_common']:
+            most_common_type['most_common'] = value
+            most_common_type['type'] = key
+
+    return (f'Products Count: {product_count} - Average Price: ${average_price} - Most Common Type: {most_common_type['type']}')
+
+
+def add_product_extra(menu: list,  *args: tuple, **kwargs: dict) -> dict:
+    id = 0
+
+    if len(menu) == 0:
+        id = 1
+    else:
+        sorted_list = sorted(menu, key=itemgetter('_id'))
+        last_product = sorted_list[-1]
+        id = last_product['_id'] + 1
+
+    for arg in args:
+        if kwargs.get(arg, False) is False:
+            raise KeyError(f'field {arg} is required')
+
+    for key in kwargs.copy():
+        if args.count(key) == 0:
+            kwargs.pop(key)
+
+    kwargs['_id'] = id
+    menu.append(kwargs)
+
     return kwargs
